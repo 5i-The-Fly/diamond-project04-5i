789ac41d406889bbb14c5b7861dcaa6d4d1817ac
Hailin <h.zeng@ufl.edu>
2023-11-26 23:23:34 -0500

    Added "Add Feedback" and "Show Rubric" buttons

--- client/src/views/Mentor/Classroom/Submissions/TeacherSubmission.jsx
+++ client/src/views/Mentor/Classroom/Submissions/TeacherSubmission.jsx
@@ -1,5 +1,40 @@
-// This is NO LONGER acrude copy of ActivityLevelReport.jsx from the researcher view. I KIND OF know what I'm doing 
-// - Brody
+// Student Submission view for teachers.
+//-------------------------------------------------------------------------------------------------
+// Observable Functions : student comment section + student submission section:
+
+// By selecting the a specific assignment from each student,
+// 1. student comment section 
+// If students left comment want to let the teacher know, their comment will be shown in the Student-comment container 
+
+// 2. student submission section:
+// their submission will shown in the Student-submission container 
+
+// Hidden Functions: Add feedback + Show Rubric
+// button 1: Add feedback
+// When teacher click the button "Add feedback" on the left hand side in the submission bar
+// A typable and editable dialog box pops up where the teacher can enter feedback, save it and send it to the student.
+
+// button 2: Show Rubric
+// When teacher click the button "Show Rubric" on the right hand side in the submission bar
+// It might trigger the display of a rubric associated with the assignment or task.
+// The rubric could include criteria for assessment, 
+// grading guidelines,
+// scores or feedback for each criterion.
+
+//-------------------------------------------------------------------------------------------------
+// TODO:
+// [ ] show student comment and submission after selecting student and specific assignment.
+// [ ] "Add feedback" Button 
+//    [ ] making a typable and editable dialog box 
+//        [ ] send feedback button
+//        [ ] attach file icon
+// [ ] "Show Rubric" Button 
+//    [ ] Thinking about what criteria should be included
+//    [ ] Make criteria Tabularization through categorization 
+//    [ ] Looking at the rubric gives teacher an idea of the student's specific score
+// 
+
+//-------------------------------------------------------------------------------------------------
 
 import React, { useEffect, useState } from 'react';
 import { Table, Select, Tag, Input, message } from 'antd';
@@ -78,24 +113,29 @@ export default function TeacherSubmission( { classroomId } ) {
           {/* Add other options related to class if needed */}
         </Select>
       </div>
+
+      <div className="comment-bar" style={{ marginTop: '100px' }}>
+        <span className="comment-text">Student Comment </span>
+      </div>
+      <div className='Student-comment' style={{ marginTop: '00px', marginBottom: '50px' }}></div>
       
 
-      <div className="top-bar" style={{ marginTop: '200px', display: 'flex', justifyContent: 'flex-start' }}>
-        <button> 
-          <div className="feedback-button" >
-            <span className="feedback-text">Add Feedback
-          </span>
+      <div className="top-bar" style={{ marginTop: '50px', display: 'flex', flexDirection:"row" }}>
+        <button style={{ marginTop: '10px', marginLeft:'30px'}}> 
+          <div className="feedback-button">
+            <span className="feedback-text">Add Feedback </span>
+          </div>
+        </button>
+
+        <button style={{ marginTop: '10px', marginRight: '30px' }}>
+          <div className='Rubric-button'>
+            <span className='Rubric-text'> Show Rubric</span>
           </div>
         </button>
       </div>
 
       <div className="Student-submission" style={{ marginTop: '0px', marginBottom: '50px' }}></div>
 
-      <div className="comment-bar" style={{ marginTop: '100px' }}>
-        <span className="comment-text">Student Comment
-        </span>
-      </div>
-      <div className='Student-comment' style={{ marginTop: '00px', marginBottom: '50px' }}></div>
 
     </div>
     );
